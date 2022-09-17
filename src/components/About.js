17d8a502a5d4fa366e0847a22f02f5066ca51f92
Helen Langridge <langridge.helen@outlook.com>
2022-09-17 16:04:45 +0100

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ const About = () => {
     return (
         <section>
             <h2>About This App</h2>
-            <p>...</p>
+            <p>Thanks for vising this World Travel Check List app.</p>
+            <p>This little project started out as a homework assignment during my time at the CodeClan professional software development bootcamp. I was struggling with my understanding of React and so I kept building upon this fun little project over subsequent weekends and I'm quite proud of where it's turned out.</p>
+            <p>Thanks to: <a href="https://restcountries.com/">REST Countries API</a>, <a href="https://react-leaflet.js.org/">Leaflet Mapping</a>, <a href="https://www.react-simple-maps.io/">React Simple Maps</a>
+            </p>
         </section>
     )
 }
